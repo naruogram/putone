8a5cb0047eb3f916bbd5d055aac4d74b40454814
naruogram <75286616+naruogram@users.noreply.github.com>
2023-07-22 23:44:56 +0900

    feat: add-color

---
+++ lib/theme/app_color_theme.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class AppColorTheme {
+  const AppColorTheme({
+    required this.mainColor,
+    required this.accentColor,
+    required this.gray100,
+    required this.gray200,
+    required this.gray300,
+    required this.gray400,
+  });
+
+  final Color mainColor;
+  final Color accentColor;
+  final Color gray100;
+  final Color gray200;
+  final Color gray300;
+  final Color gray400;
+
+  factory AppColorTheme.dark() => AppColorTheme(
+        mainColor: Color(0xFFB800),
+        accentColor: Color(0x27A2BB),
+        gray100: Color(0x404040),
+        gray200: Color(0x616161),
+        gray300: Color(0xD9D9D9),
+        gray400: Color(0xF0F0F0),
+      );
+}
