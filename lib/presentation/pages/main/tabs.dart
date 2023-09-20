a3dc7de19d8b7af8f1c5817dc073b5e1b26d602c
yugo yoshida <yugo.yoshida@gmail.com>
2023-09-20 13:04:20 +0900

    ScaffoldのappBarを削除

--- lib/presentation/pages/main/tabs.dart
+++ lib/presentation/pages/main/tabs.dart
@@ -14,16 +14,16 @@ class Tabs extends StatefulWidget {
 
 class _TabsState extends State<Tabs> {
   int _currentIndex = 0;
+
+  //bottomNavigationBarのitemを押した時にbodyに入れるwidgetを変化するためにwidgetをリスト化
   late List _currentContent = [
     HomeScreen(),
     CommunityScreen(),
   ];
-  late List _appBarList;
 
   @override
   void initState() {
     super.initState();
-    _appBarList = [_homeAppBar, _community_appbar];
   }
 
   void _onBottomBarTapped(int index) {
@@ -32,65 +32,9 @@ class _TabsState extends State<Tabs> {
     });
   }
 
-  final Widget _homeAppBar = AppBar(
-    backgroundColor: AppColorTheme.dark().mainColor,
-    elevation: 0,
-    title: Text(
-      'Putone', //各bottomNavBarのindexに合わせて変更する
-      style: GoogleFonts.getFont('Carter One'),
-    ),
-    actions: [
-      IconButton(
-        onPressed: () {},
-        icon: Icon(Icons.library_music_outlined),
-      ),
-      IconButton(
-        onPressed: () {},
-        icon: Icon(Icons.notifications_none),
-      ),
-      IconButton(
-        onPressed: () {},
-        icon: Icon(Icons.chat),
-      ),
-    ],
-  );
-
-  final Widget _community_appbar = AppBar(
-    backgroundColor: AppColorTheme.dark().mainColor,
-    elevation: 0,
-    title: Text(
-      'フレンド追加',
-      style: GoogleFonts.inter(
-        fontWeight: FontWeight.w900,
-        fontSize: 24,
-      ),
-    ),
-    actions: [
-      Container(
-        padding: EdgeInsets.symmetric(vertical: 8),
-        margin: EdgeInsets.fromLTRB(0, 0, 8, 0),
-        child: ElevatedButton(
-          onPressed: () {},
-          child: Text(
-            'ID検索',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 16,
-            ),
-          ),
-          style: TextButton.styleFrom(
-            backgroundColor: Colors.black,
-            fixedSize: Size.fromHeight(30),
-          ),
-        ),
-      )
-    ],
-  );
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //appBar: _appBarList[_currentIndex], //_currentIndexによって表示するAppBarを変更する
       body: _currentContent[_currentIndex], //_currentIndexによって表示するBodyを変更する
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: AppColorTheme.dark().mainColor,
